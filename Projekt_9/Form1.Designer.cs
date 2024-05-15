f5c3c19e17267c163777c6cc7bbc68f715b3f551
Stacjonarka <mjwoj@DESKTOP-4R65IHO>
2024-05-15 23:08:39 +0200

    Szkic

--- Projekt_9/Form1.Designer.cs
+++ Projekt_9/Form1.Designer.cs
@@ -28,13 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Document = new System.Windows.Forms.PictureBox();
+            this.nr_albumu = new System.Windows.Forms.Label();
+            this.imieNazwisko = new System.Windows.Forms.Label();
+            this.semest_rok = new System.Windows.Forms.Label();
+            this.kierunek = new System.Windows.Forms.Label();
+            this.dzien_gora = new System.Windows.Forms.Label();
+            this.przedmiot = new System.Windows.Forms.Label();
+            this.punkty = new System.Windows.Forms.Label();
+            this.prowadzacy = new System.Windows.Forms.Label();
+            this.uzasadnienie = new System.Windows.Forms.Label();
+            this.komisja1 = new System.Windows.Forms.Label();
+            this.komisja2 = new System.Windows.Forms.Label();
+            this.komisja3 = new System.Windows.Forms.Label();
+            this.dzien_dol = new System.Windows.Forms.Label();
+            this.podpisKomisji = new System.Windows.Forms.Label();
+            this.podpisStudenta = new System.Windows.Forms.Label();
+            this.zapisDoBazy = new System.Windows.Forms.Button();
+            this.odczytZBazy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Document)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Document
+            // 
+            this.Document.Image = global::Projekt_9.Properties.Resources.Wniosek_o_egzamin_komisyjny_1;
+            this.Document.Location = new System.Drawing.Point(-1, -3);
+            this.Document.Name = "Document";
+            this.Document.Size = new System.Drawing.Size(683, 1063);
+            this.Document.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Document.TabIndex = 0;
+            this.Document.TabStop = false;
+            // 
+            // nr_albumu
+            // 
+            this.nr_albumu.AutoSize = true;
+            this.nr_albumu.Location = new System.Drawing.Point(184, 147);
+            this.nr_albumu.Name = "nr_albumu";
+            this.nr_albumu.Size = new System.Drawing.Size(35, 13);
+            this.nr_albumu.TabIndex = 1;
+            this.nr_albumu.Text = "label1";
+            // 
+            // imieNazwisko
+            // 
+            this.imieNazwisko.AutoSize = true;
+            this.imieNazwisko.Location = new System.Drawing.Point(184, 186);
+            this.imieNazwisko.Name = "imieNazwisko";
+            this.imieNazwisko.Size = new System.Drawing.Size(35, 13);
+            this.imieNazwisko.TabIndex = 2;
+            this.imieNazwisko.Text = "label2";
+            // 
+            // semest_rok
+            // 
+            this.semest_rok.AutoSize = true;
+            this.semest_rok.Location = new System.Drawing.Point(184, 221);
+            this.semest_rok.Name = "semest_rok";
+            this.semest_rok.Size = new System.Drawing.Size(35, 13);
+            this.semest_rok.TabIndex = 3;
+            this.semest_rok.Text = "label3";
+            // 
+            // kierunek
+            // 
+            this.kierunek.AutoSize = true;
+            this.kierunek.Location = new System.Drawing.Point(184, 258);
+            this.kierunek.Name = "kierunek";
+            this.kierunek.Size = new System.Drawing.Size(35, 13);
+            this.kierunek.TabIndex = 4;
+            this.kierunek.Text = "label4";
+            // 
+            // dzien_gora
+            // 
+            this.dzien_gora.AutoSize = true;
+            this.dzien_gora.Location = new System.Drawing.Point(513, 136);
+            this.dzien_gora.Name = "dzien_gora";
+            this.dzien_gora.Size = new System.Drawing.Size(35, 13);
+            this.dzien_gora.TabIndex = 5;
+            this.dzien_gora.Text = "label5";
+            // 
+            // przedmiot
+            // 
+            this.przedmiot.AutoSize = true;
+            this.przedmiot.Location = new System.Drawing.Point(155, 458);
+            this.przedmiot.Name = "przedmiot";
+            this.przedmiot.Size = new System.Drawing.Size(35, 13);
+            this.przedmiot.TabIndex = 6;
+            this.przedmiot.Text = "label6";
+            // 
+            // punkty
+            // 
+            this.punkty.AutoSize = true;
+            this.punkty.Location = new System.Drawing.Point(485, 458);
+            this.punkty.Name = "punkty";
+            this.punkty.Size = new System.Drawing.Size(35, 13);
+            this.punkty.TabIndex = 7;
+            this.punkty.Text = "label7";
+            // 
+            // prowadzacy
+            // 
+            this.prowadzacy.AutoSize = true;
+            this.prowadzacy.Location = new System.Drawing.Point(338, 479);
+            this.prowadzacy.Name = "prowadzacy";
+            this.prowadzacy.Size = new System.Drawing.Size(35, 13);
+            this.prowadzacy.TabIndex = 8;
+            this.prowadzacy.Text = "label8";
+            // 
+            // uzasadnienie
+            // 
+            this.uzasadnienie.AutoSize = true;
+            this.uzasadnienie.Location = new System.Drawing.Point(291, 527);
+            this.uzasadnienie.Name = "uzasadnienie";
+            this.uzasadnienie.Size = new System.Drawing.Size(35, 13);
+            this.uzasadnienie.TabIndex = 9;
+            this.uzasadnienie.Text = "label9";
+            // 
+            // komisja1
+            // 
+            this.komisja1.AutoSize = true;
+            this.komisja1.Location = new System.Drawing.Point(100, 698);
+            this.komisja1.Name = "komisja1";
+            this.komisja1.Size = new System.Drawing.Size(41, 13);
+            this.komisja1.TabIndex = 10;
+            this.komisja1.Text = "label10";
+            // 
+            // komisja2
+            // 
+            this.komisja2.AutoSize = true;
+            this.komisja2.Location = new System.Drawing.Point(100, 711);
+            this.komisja2.Name = "komisja2";
+            this.komisja2.Size = new System.Drawing.Size(41, 13);
+            this.komisja2.TabIndex = 11;
+            this.komisja2.Text = "label11";
+            // 
+            // komisja3
+            // 
+            this.komisja3.AutoSize = true;
+            this.komisja3.Location = new System.Drawing.Point(100, 724);
+            this.komisja3.Name = "komisja3";
+            this.komisja3.Size = new System.Drawing.Size(41, 13);
+            this.komisja3.TabIndex = 12;
+            this.komisja3.Text = "label12";
+            // 
+            // dzien_dol
+            // 
+            this.dzien_dol.AutoSize = true;
+            this.dzien_dol.Location = new System.Drawing.Point(155, 816);
+            this.dzien_dol.Name = "dzien_dol";
+            this.dzien_dol.Size = new System.Drawing.Size(41, 13);
+            this.dzien_dol.TabIndex = 13;
+            this.dzien_dol.Text = "label13";
+            // 
+            // podpisKomisji
+            // 
+            this.podpisKomisji.AutoSize = true;
+            this.podpisKomisji.Location = new System.Drawing.Point(485, 816);
+            this.podpisKomisji.Name = "podpisKomisji";
+            this.podpisKomisji.Size = new System.Drawing.Size(41, 13);
+            this.podpisKomisji.TabIndex = 14;
+            this.podpisKomisji.Text = "label14";
+            // 
+            // podpisStudenta
+            // 
+            this.podpisStudenta.AutoSize = true;
+            this.podpisStudenta.Location = new System.Drawing.Point(485, 582);
+            this.podpisStudenta.Name = "podpisStudenta";
+            this.podpisStudenta.Size = new System.Drawing.Size(41, 13);
+            this.podpisStudenta.TabIndex = 15;
+            this.podpisStudenta.Text = "label15";
+            // 
+            // zapisDoBazy
+            // 
+            this.zapisDoBazy.Location = new System.Drawing.Point(103, 899);
+            this.zapisDoBazy.Name = "zapisDoBazy";
+            this.zapisDoBazy.Size = new System.Drawing.Size(94, 42);
+            this.zapisDoBazy.TabIndex = 16;
+            this.zapisDoBazy.Text = "Zapis";
+            this.zapisDoBazy.UseVisualStyleBackColor = true;
+            // 
+            // odczytZBazy
+            // 
+            this.odczytZBazy.Location = new System.Drawing.Point(453, 900);
+            this.odczytZBazy.Name = "odczytZBazy";
+            this.odczytZBazy.Size = new System.Drawing.Size(95, 41);
+            this.odczytZBazy.TabIndex = 17;
+            this.odczytZBazy.Text = "Odczyt";
+            this.odczytZBazy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 1061);
+            this.Controls.Add(this.odczytZBazy);
+            this.Controls.Add(this.zapisDoBazy);
+            this.Controls.Add(this.podpisStudenta);
+            this.Controls.Add(this.podpisKomisji);
+            this.Controls.Add(this.dzien_dol);
+            this.Controls.Add(this.komisja3);
+            this.Controls.Add(this.komisja2);
+            this.Controls.Add(this.komisja1);
+            this.Controls.Add(this.uzasadnienie);
+            this.Controls.Add(this.prowadzacy);
+            this.Controls.Add(this.punkty);
+            this.Controls.Add(this.przedmiot);
+            this.Controls.Add(this.dzien_gora);
+            this.Controls.Add(this.kierunek);
+            this.Controls.Add(this.semest_rok);
+            this.Controls.Add(this.imieNazwisko);
+            this.Controls.Add(this.nr_albumu);
+            this.Controls.Add(this.Document);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Document)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Document;
+        private System.Windows.Forms.Label nr_albumu;
+        private System.Windows.Forms.Label imieNazwisko;
+        private System.Windows.Forms.Label semest_rok;
+        private System.Windows.Forms.Label kierunek;
+        private System.Windows.Forms.Label dzien_gora;
+        private System.Windows.Forms.Label przedmiot;
+        private System.Windows.Forms.Label punkty;
+        private System.Windows.Forms.Label prowadzacy;
+        private System.Windows.Forms.Label uzasadnienie;
+        private System.Windows.Forms.Label komisja1;
+        private System.Windows.Forms.Label komisja2;
+        private System.Windows.Forms.Label komisja3;
+        private System.Windows.Forms.Label dzien_dol;
+        private System.Windows.Forms.Label podpisKomisji;
+        private System.Windows.Forms.Label podpisStudenta;
+        private System.Windows.Forms.Button zapisDoBazy;
+        private System.Windows.Forms.Button odczytZBazy;
     }
 }
 
